4a9dd9dfda681f33a72869f7501ff96bd5715a00
nirobdev9 <smnnirob839@gmail.com>
2025-03-09 10:19:18 +0600

    added history style and an alert

--- js/task.js
+++ js/task.js
@@ -7,6 +7,9 @@ let taskTitle = document.querySelectorAll(".task-title");
 
 let convertedCompletedTask = parseInt(completedTask.innerText);
 let convertedTaskAssigned = parseInt(taskAssigned.innerText);
+let totalTask = convertedCompletedTask + convertedTaskAssigned;
+console.log(totalTask)
+
 
 for (let button of buttons) {
   button.addEventListener("click", function (event) {
@@ -22,10 +25,14 @@ for (let button of buttons) {
     const taskTitleText = taskTitle[taskIndex].innerText; 
     const currentTime = new Date().toLocaleString();
 
-
-
     const p = document.createElement("p");
     p.innerText = `Task ${taskTitleText} at ${currentTime} completed`;
+    p.classList.add("bg-[rgba(55,82,253,0.2)]", "p-4", "mt-2", "rounded-xl");
     historySection.appendChild(p);
+
+    if (convertedCompletedTask === totalTask){
+      alert("all task completed")
+    }
+
   });
 }
